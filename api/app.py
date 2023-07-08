be89ffb09dbf5d5a270f58ae06dd45b77e998c3e
Ingo Schriek <ingoqschriek@gmail.com>
2023-07-08 17:45:11 -0300

    .

--- api/app.py
+++ api/app.py
@@ -117,10 +117,6 @@ def login():
     if session.get("email"):
         return redirect(url_for(".index"))
 
-    # if request.args.get('d') == 'demo':
-    #     form.email = "test@example.com"
-    #     form.password = "test"
-
     form = LoginForm()
     if form.validate_on_submit():
         
